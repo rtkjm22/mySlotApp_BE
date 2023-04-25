ace8b43dd1042e54d657806905dda63ff57918bd
ryotak1998 <74517286+rtkjm22@users.noreply.github.com>
2023-04-25 23:40:36 +0900

    ADD:JWTによる認証処理追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,7 @@
 Rails.application.routes.draw do
+  resources :users, only: %i[new create show edit update]
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#new'
 end
